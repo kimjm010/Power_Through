8849e64136ff29b643ee18f282ba118d7957baaf
Chris Kim <kimjm010@icloud.com>
2023-03-16 16:49:04 +0900

    [Power_Through] Initial Commit

---
+++ Power_Through/Power_Through/Power_ThroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_ThroughApp.swift
+//  Power_Through
+//
+//  Created by Chris Kim on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Power_ThroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
